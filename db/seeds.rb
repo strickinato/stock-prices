79b3facbc259b37abf7282db8371f19877b36e93
App Academy Student <student@appacademy.io>
2015-01-14 14:48:37 -0800

    Added jquery turbolinks gem to fix issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.new({username: "aaronstrick", password: "hello123"})
+user.save!
+
+Company.create!({ ticker: "AAPL" })
+Company.create!({ ticker: "GOOG" })
+Company.create!({ ticker: "AARO" })
+
+UsersCompany.create!({ user_id: 1, company_id: 1 })
+UsersCompany.create!({ user_id: 1, company_id: 2 })
+
+Price.create!({ company_id: 1, date:"2014-01-10", price: 45 })
+Price.create!({ company_id: 1, date:"2014-01-11", price: 43 })
+Price.create!({ company_id: 2, date:"2014-01-10", price: 599 })
+Price.create!({ company_id: 2, date:"2014-01-11", price: 504 })
+Price.create!({ company_id: 3, date:"2014-01-10", price: 1 })
+Price.create!({ company_id: 3, date:"2014-01-11", price: 2 })
